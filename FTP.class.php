8ba8038ba99a52aa170705e55fdef57826485d69
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-22 10:48:33 +0900

    New: FTP :: Auto( array $config )

--- FTP.class.php
+++ FTP.class.php
@@ -35,4 +35,42 @@ class FTP implements IF_CD
 	 *
 	 */
 	use OP_CORE, OP_CI;
+
+	/**	Automatically
+	 *
+	 * @created    2026-04-19
+	 */
+	static function Auto( array $config )
+	{
+		//	...
+		$host     = $config['host'];
+		$user     = $config['user'];
+		$password = $config['password'];
+		$passive  = $config['passive'];
+		$local    = $config['local'];
+		$remote   = $config['remote'];
+
+		//	Check password exists.
+		if(!$password ){
+			$password = self::GetPassword();
+		}
+
+		//	...
+		$host     = escapeshellarg($host);
+		$local    = escapeshellarg($local);
+		$remote   = escapeshellarg($remote);
+		$login    = escapeshellarg("{$user},{$password}");
+
+		//	...
+		$exclude = '--exclude-glob=.* --exclude-glob=_* --exclude=.git';
+
+		//	...
+		$commands[] = $passive ? 'set ftp:passive-mode on': 'set ftp:passive-mode off';
+		$commands[] = "mirror -R --parallel=4 --dereference {$exclude} {$local} {$remote}";
+		$commands[] = "quit";
+		$commands = implode('; ', $commands);
+		$comand = "lftp -u {$login} {$host} -e \"{$commands}\"";
+	//	D($comand);
+		passthru($comand);
+	}
 }
